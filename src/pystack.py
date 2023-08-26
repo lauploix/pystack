158dc9122a4ce3b763ae3d607824ef80a4a3005b
Laurent Ploix <laurent.ploix@gmail.com>
2023-08-26 10:41:17 +0200

    Update pystack.py

--- src/pystack.py
+++ src/pystack.py
@@ -5,7 +5,7 @@ import ast
 def entries(entry_string):
     for entry in entry_string.strip().split(" "): yield entry
 
-def pystack():
+def pystack(input_provider):
     """ROLL
 Moves a specified level to level 1
 ( e.g. 4 ROLL will move level 4 to level 1 )
@@ -19,12 +19,11 @@ Copies a specified number of levels again
 (e.g. 6 DUPN will copy the first six levels of the stack again)
 """
 
-
     stack = []
     while (True):
         print (stack)
         value = None
-        for entry in entries(input()):
+        for entry in entries(input_provider()):
             if entry == "+":
                 last = stack.pop()
                 previous = stack.pop()
@@ -84,4 +83,4 @@ Copies a specified number of levels again
 
 
 if __name__ == "__main__":
-    pystack()
+    pystack(input)
