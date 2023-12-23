f844e10be6179c97455411e0525b7d68419f84c4
Laurent Ploix <laurent.ploix@gmail.com>
2023-12-23 11:47:52 +0100

    Version with a RpnStack object, early

--- src/pystack.py
+++ src/pystack.py
@@ -1,6 +1,118 @@
 import ast
 
-## Inspired by http://www.quickclose.com.au/tut2.htm
+# Inspired by http://www.quickclose.com.au/tut2.htm
+
+
+class StackException(Exception):
+    pass
+
+
+class RpnStack:
+    def __init__(self, iterable=None):
+        self.__stack = []
+        if iterable:
+            self.__stack.extend(iterable)
+        self.exec_map = {
+            "size": "_exec_size",
+            "+": "_exec_plus",
+            "-": "_exec_minus",
+            "*": "_exec_mult",
+            "/": "_exec_div",
+            "//": "_exec_divdiv",
+            "%": "_exec_percent",
+        }
+        self.msg = ""
+
+    def __len__(self):
+        return len(self.__stack)
+
+    def size(self):
+        return len(self.__stack)
+
+    def pop(self):
+        return self.__stack.pop()
+
+    def top(self):
+        return self.__stack[-1]
+
+    def push(self, value):
+        self.__stack.append(value)
+
+    # Tries to turn the last element into an operation
+    # If the operation is not found, it is pushed back onto the stack
+    # and raise an error
+    # if the operation is found, it is executed
+    # If the operation is a literal, it is pushed onto the stack
+    def exec(self, tokens_string=None, token=None, tokens=None):
+        # TODO: fix this algorithm to take into account the strings
+        # with spaces in them
+        self.msg = ""
+
+        if tokens_string is not None:
+            tokens = tokens_string.split(" ")
+
+        if tokens is not None:
+            for token in tokens:
+                self.exec(token=token)
+            return
+
+        token = token or self.pop()
+
+        f_name = self.exec_map.get(token.lower())
+        if f_name:
+            getattr(self, f_name)()
+        else:
+            try:
+                value = ast.literal_eval(token)
+                self.__stack.append(value)
+            except SyntaxError:
+                self.msg = "Syntax Error: " + token
+                return
+            except ValueError:
+                self.msg = "Operation not found: " + token
+
+    def __pop_as_list(self, n):
+        if len(self) >= n:
+            return [self.__stack.pop() for _ in range(n)][::-1]
+        raise StackException()
+
+    # execute the size operation
+    def _exec_size(self):
+        self.push(self.size())
+
+    # execute the plus operation
+    def _exec_plus(self):
+        (a, b) = self.__pop_as_list(2)
+        self.push(a + b)
+
+    # execute the mult operation
+    def _exec_mult(self):
+        (a, b) = self.__pop_as_list(2)
+        self.push(a * b)
+
+    # execute the div operation
+    def _exec_div(self):
+        (a, b) = self.__pop_as_list(2)
+        self.push(a / b)
+
+    # execute the divdiv operation
+    def _exec_divdiv(self):
+        (a, b) = self.__pop_as_list(2)
+        self.push(a // b)
+
+    # execute the percent operation
+    def _exec_percent(self):
+        (a, b) = self.__pop_as_list(2)
+        self.push(a % b)
+
+    # execute the minus operation
+    def _exec_minus(self):
+        (a, b) = self.__pop_as_list(2)
+        self.push(a - b)
+
+    # dups the content of the stack, element by element
+    def __iter__(self):
+        return iter(self.__stack)
 
 
 def entries(entry_string):
@@ -16,10 +128,12 @@ def pystack(input_provider):
     Moves level 1 to a specified level
     ( e.g. 7 ROLLD will move level 1 to level 7 )
     DEPTH
-    Counts the number of active levels in the stack placing the number on level 1
+    Counts the number of active levels in the stack placing
+    the number on level 1
     DUPN
     Copies a specified number of levels again
-    (e.g. 6 DUPN will copy the first six levels of the stack again)"""
+    (e.g. 6 DUPN will copy the first six levels of the
+    stack again)"""
 
     stack = []
     print(stack)
--- tests/test_pystack.py
+++ tests/test_pystack.py
@@ -1,11 +1,128 @@
-from pystack import pystack
+from pystack import RpnStack
 
-def test_int_add():
-    s = pystack(["'abc'", "1", "3", "+"])
-    assert s == ["abc", 4]
 
-def test_int_minus():
-    s = pystack(["'abc'", "1", "3", "-"])
-    print (s)
-    assert s == ["abc", -2]
+def test_create_empty_stack():
+    RpnStack()
 
+
+def test_create_stack_from_list():
+    s = RpnStack((1, 2, 4))
+    assert len(s) == 3
+    assert s.size() == 3
+    assert s.top() == 4
+
+
+def test_iter():
+    s = RpnStack((1, 2, 4))
+    assert list(s) == [1, 2, 4]
+
+
+def test_operation_size_on_empty_stack():
+    s = RpnStack(())
+    s.exec("size")
+    # Now contains one element: the size as computed
+    assert len(s) == 1
+    # the top element of the stack is 0 (it's size, when evaluated)
+    assert s.top() == 0
+
+
+def test_operation_size_with_elements():
+    s = RpnStack((1, "abc", object()))
+    s.exec("size")
+    # Now contains 4 elements: the size as computed
+    assert len(s) == 4
+    # the top element of the stack is 3, ie the size of the stack when computed
+    assert s.top() == 3
+    s._exec_size()
+    assert len(s) == 5
+    assert s.top() == 4
+
+
+def test_exec_adds_strings_to_stack():
+    s = RpnStack()
+    s.exec("'abc'")
+    assert len(s) == 1
+    assert s.top() == "abc"
+
+
+def test_exec_adds_multiple_values_on_stack():
+    s = RpnStack()
+    s.exec("1 'abc'")
+    assert len(s) == 2
+    assert list(s) == [1, "abc"]
+
+
+def test_exec_gets_exception_with_unclosed_str():
+    s = RpnStack()
+    s.exec("'abc")
+    assert s.msg == "Syntax Error: 'abc"
+    assert len(s) == 0
+
+
+def test_pop():
+    s = RpnStack((1, 2, 3))
+    assert s.pop() == 3
+    assert len(s) == 2
+    assert s.top() == 2
+
+
+def test_exec_parses_as_far_as_it_can():
+    s = RpnStack()
+    s.exec("1 'abc")
+    assert list(s) == [1]
+
+
+def test_exec_parses_unknown_op():
+    s = RpnStack()
+    s.exec("1 abc")
+    assert list(s) == [1]
+    assert s.msg == "Operation not found: abc"
+
+
+# Test + on ints
+def test_plus_int():
+    s = RpnStack()
+    s.exec("'a' 1 2 +")
+    assert list(s) == ["a", 3]
+
+
+# Test + on strings
+def test_plus_str():
+    s = RpnStack()
+    s.exec("'a' 'b' 'c' +")
+    assert list(s) == ["a", "bc"]
+
+
+# Test -
+def test_minus_on_ints():
+    s = RpnStack()
+    s.exec("'a' 5 7 -")
+    assert list(s) == ["a", -2]
+
+
+# Test / on ints
+def test_div_on_ints():
+    s = RpnStack()
+    s.exec("'a' 5 2 /")
+    assert list(s) == ["a", 2.5]
+
+
+# Test / on ints
+def test_divdiv_on_ints():
+    s = RpnStack()
+    s.exec("'a' 5 2 //")
+    assert list(s) == ["a", 2]
+
+
+# Test / on ints
+def test_percent_on_ints():
+    s = RpnStack()
+    s.exec("'a' 5 2 %")
+    assert list(s) == ["a", 1]
+
+
+# Test * on ints
+def test_mult_on_ints():
+    s = RpnStack()
+    s.exec("'a' 5 2 *")
+    assert list(s) == ["a", 10]
