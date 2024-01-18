123b60a8a8efada818cd32283fe574ce294dfe2d
Laurent Ploix <laurent.ploix@gmail.com>
2024-01-18 11:55:02 +0100

    Infer _exec_X from name

--- src/pystack.py
+++ src/pystack.py
@@ -1,65 +1,40 @@
 import ast
 
+import numpy as np
+
 # Inspired by http://www.quickclose.com.au/tut2.htm
 
 
 class StackException(Exception):
-    pass
-
-
-def token_finder(token_str):
-    SQ, DQ = "'", '"'
-    quote = None
-    current_token = ""
-    for token in token_str.split(" "):
-        if quote is None:
-            if token[0] in (SQ, DQ):
-                if token[-1] == token[0] and len(token) > 1:
-                    yield token
-                else:
-                    quote = token[0]
-                    current_token = token
-            else:
-                yield token
-        else:
-            current_token += " " + token
-            if quote and token.endswith(quote):
-                quote = None
-                yield current_token
-    if quote:
-        raise StackException("Unclosed string")
+    def __init__(self, msg="Unknown Exception", inner=None):
+        self.msg = msg
+        self.inner = inner
+        super().__init__(msg)
 
 
 class RpnStack:
-    def __init__(self, iterable=None):
+    def __init__(self, tokens=None, raises=False):
         self.__stack = []
-        if iterable:
-            self.__stack.extend(iterable)
+        self.raises = raises
+        if tokens:
+            self.__stack.extend(tokens)
         self.exec_map = {
-            "size": self._exec_size,
             "+": self._exec_plus,
             "-": self._exec_minus,
             "*": self._exec_mult,
             "**": self._exec_power,
             "/": self._exec_div,
-            "//": self._exec_divdiv,
-            "%": self._exec_percent,
-            "swap": self._exec_swap,
-            "pick": self._exec_pick,
-            "drop": self._exec_drop,
-            "drop2": self._exec_drop2,
-            "drpn": self._exec_drpn,
-            "dup": self._exec_dup,
-            "dup2": self._exec_dup2,
-            "dupn": self._exec_dupn,
-            "over": self._exec_over,
-            "rot": self._exec_rot,
-            "roll": self._exec_roll,
-            "rolld": self._exec_rolld,
-            "clear": self._exec_clear,
-            "depth": self._exec_depth,
+            "//": self._exec_floordiv,
+            "%": self._exec_mod,
         }
-        self.msg = ""
+        # From numpy, list fucntions (numpy.ufunc),
+        # find the ones that are not deprecated
+        # and add them to the exec_map togther with their
+        # argument number
+
+    @property
+    def msg(self):
+        return self.exception and self.exception.msg or ""
 
     def __len__(self):
         return len(self.__stack)
@@ -81,42 +56,75 @@ class RpnStack:
     # and raise an error
     # if the operation is found, it is executed
     # If the operation is a literal, it is pushed onto the stack
-    def exec(self, tokens_string=None, token=None, tokens=None):
+    def exec(self, token=None, tokens=None):
         # TODO: fix this algorithm to take into account the strings
         # with spaces in them
-        self.msg = ""
-
-        if tokens_string is not None:
-            if tokens_string == "":
-                return
-            try:
-                tokens = list(token_finder(tokens_string))
-            except StackException as e:
-                self.msg = str(e)
-                return
+        self.exception = None
 
         if tokens is not None:
             for token in tokens:
+                # Try to execute the token, one by one.
+                # There is no transaction
                 self.exec(token=token)
-            return
-
-        token = token or self.pop()
-
-        f_name = self.exec_map.get(token.lower())
-        if f_name:
-            if type(f_name) is str:
-                getattr(self, f_name)()
-            else:
-                f_name()
         else:
+            f = None  # tell if we have found the operation
             try:
-                value = ast.literal_eval(token)
-                self.__stack.append(value)
-            except SyntaxError:
-                self.msg = "Syntax Error: " + token
-                return
-            except ValueError:
-                self.msg = "Operation not found: " + token
+                # self.pop() will execute the last element of the stack
+                # as if its string was entered as a command
+                # Is this what I want?
+                token = token or self.pop()
+
+                # First we check is there is a registered function
+                # or an _exec_ function
+                f = self.exec_map.get(token.lower()) or getattr(
+                    self, f"_exec_{token.lower()}", None
+                )
+                if not f:
+                    # If not, we try to find a numpy function
+                    f = getattr(np, token.lower(), None)
+                    if f and type(f) is not np.ufunc:
+                        # We only wants the functions,
+                        # not the constants or classes
+                        f = None
+                # If we found a function, we execute it
+                if f and type(f) is np.ufunc:
+                    # Nb of arguments it takes
+                    n_args = f.nin
+                    args = self.__pop_as_list(n_args)
+                    self.push(f(*args))
+                elif f:
+                    f()
+                else:
+                    # Otherwise we try to evaluate the token as a
+                    # python literal
+                    # Therefore must be a string, a number, a list,
+                    # a tuple, a dict...
+                    value = ast.literal_eval(token)
+                    self.__stack.append(value)
+            except IndexError as e:
+                self.exception = StackException(
+                    f"Index error with operation {token}: {e}"
+                )
+            except SyntaxError as e:
+                self.exception = StackException(
+                    f"Syntax Error: {token}", inner=e
+                )
+                self.exception
+            except ValueError as e:
+                if not f:
+                    self.exception = StackException(
+                        f"Operation not found: {token}", inner=e
+                    )
+                else:
+                    self.exception = StackException(
+                        f"Error while executing: {token}", inner=e
+                    )
+
+            except Exception as e:
+                self.exception = StackException(str(e), inner=e)
+            finally:
+                if self.exception and self.raises:
+                    raise self.exception from self.exception.inner
 
     def __pop_as_list(self, n):
         if len(self) >= n:
@@ -129,9 +137,25 @@ class RpnStack:
         self.push(b)
         self.push(a)
 
+    # execute the swap operation
+    def _exec_call(self):
+        (target, params) = self.__pop_as_list(2)
+        result = target(*params)
+        self.push(result)
+
+    def _exec_e(self):
+        self.push(np.e)
+
+    def _exec_pi(self):
+        self.push(np.pi)
+
     # execute the size operation
     def _exec_size(self):
         self.push(self.size())
+
+    # evaluate the string in python
+    def _exec_eval(self):
+        self.push(eval(self.pop()))
 
     # execute the plus operation
     def _exec_plus(self):
@@ -153,13 +177,13 @@ class RpnStack:
         (a, b) = self.__pop_as_list(2)
         self.push(a / b)
 
-    # execute the divdiv operation
-    def _exec_divdiv(self):
+    # execute the floordiv operation
+    def _exec_floordiv(self):
         (a, b) = self.__pop_as_list(2)
         self.push(a // b)
 
     # execute the percent operation
-    def _exec_percent(self):
+    def _exec_mod(self):
         (a, b) = self.__pop_as_list(2)
         self.push(a % b)
 
@@ -190,8 +214,7 @@ class RpnStack:
 
     # execute the dupn
     def _exec_dupn(self):
-        n = self.pop()
-        self.__stack.extend(self.__stack[-n:])
+        self.__stack.extend(self.__stack[-self.pop() :])
 
     # execute the over
     def _exec_over(self):
@@ -200,6 +223,14 @@ class RpnStack:
     # execute the drop operation
     def _exec_drop(self):
         self.__stack.pop()
+
+    # execute the array operation, transformaing the data into numpy ndarray
+    def _exec_array(self):
+        # case; entry is a list or tuple
+        values = self.__stack.pop()
+        out = np.array(values)
+        # shape = (len(values), 1)
+        self.push(out)
 
     # execute the drop2
     def _exec_drop2(self):
@@ -239,7 +270,7 @@ def entries(entry_string):
 
 
 if __name__ == "__main__":
-    s = RpnStack()
+    s = RpnStack(raises=True)
 
     def input_provider():
         while True:
@@ -247,7 +278,8 @@ if __name__ == "__main__":
 
     print(list(s))
     for i in input_provider():
-        s.exec(i)
-        if s.msg:
-            print(s.msg)
+        try:
+            s.exec(i)
+        except StackException as e:
+            print(e.msg)
         print(list(s))
--- tests/test_pystack.py
+++ tests/test_pystack.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pytest
+
 from pystack import RpnStack
 
 
@@ -29,13 +32,12 @@ def test_iter():
     assert list(s) == [1, 2, 4]
 
 
-def test_operation_size_on_empty_stack():
-    s = RpnStack(())
-    s.exec("size")
+def test_operation_size_on_empty_stack(stack):
+    stack.exec("size")
     # Now contains one element: the size as computed
-    assert len(s) == 1
+    assert len(stack) == 1
     # the top element of the stack is 0 (it's size, when evaluated)
-    assert s.top() == 0
+    assert stack.top() == 0
 
 
 def test_operation_size_with_elements():
@@ -59,7 +61,7 @@ def test_exec_adds_strings_to_stack():
 
 def test_exec_adds_multiple_values_on_stack():
     s = RpnStack()
-    s.exec("1 'abc'")
+    s.exec(tokens="1 'abc'".split())
     assert len(s) == 2
     assert list(s) == [1, "abc"]
 
@@ -81,166 +83,283 @@ def test_exec_parses_as_far_as_it_can():
     s = RpnStack()
     s.exec("1 'abc")
     assert list(s) == []
-    assert s.msg == "Unclosed string"
+    assert s.msg == "Syntax Error: 1 'abc"
 
 
 def test_exec_parses_unknown_op():
     s = RpnStack()
-    s.exec("1 abc")
+    s.exec(tokens="1 abc".split())
     assert list(s) == [1]
     assert s.msg == "Operation not found: abc"
-
-
-def test_scan_with_strings_and_space():
-    s = RpnStack()
-    s.exec("'a b' \"c d\"")
-    assert list(s) == ["a b", "c d"]
 
 
 # Test + on ints
 def test_plus_int():
     s = RpnStack()
-    s.exec("'a' 1 2 +")
+    s.exec(tokens="'a' 1 2 +".split())
     assert list(s) == ["a", 3]
 
 
 # Test + on strings
 def test_plus_str():
     s = RpnStack()
-    s.exec("'a' 'b' 'c' +")
+    s.exec(tokens="'a' 'b' 'c' +".split())
     assert list(s) == ["a", "bc"]
 
 
 # Test -
 def test_minus_on_ints():
     s = RpnStack()
-    s.exec("'a' 5 7 -")
+    s.exec(tokens="'a' 5 7 -".split())
     assert list(s) == ["a", -2]
 
 
 # Test / on ints
 def test_div_on_ints():
     s = RpnStack()
-    s.exec("'a' 5 2 /")
+    s.exec(tokens="'a' 5 2 /".split())
     assert list(s) == ["a", 2.5]
 
 
 # Test / on ints
 def test_divdiv_on_ints():
     s = RpnStack()
-    s.exec("'a' 5 2 //")
+    s.exec(tokens="'a' 5 2 //".split())
     assert list(s) == ["a", 2]
 
 
 # Test / on ints
 def test_percent_on_ints():
     s = RpnStack()
-    s.exec("'a' 5 2 %")
+    s.exec(tokens="'a' 5 2 %".split())
     assert list(s) == ["a", 1]
 
 
 # Test * on ints
 def test_mult_on_ints():
     s = RpnStack()
-    s.exec("'a' 5 2 *")
+    s.exec(tokens="'a' 5 2 *".split())
     assert list(s) == ["a", 10]
 
 
 # Test ** on ints
 def test_power_on_ints():
     s = RpnStack()
-    s.exec("'a' 5 2 **")
+    s.exec(tokens="'a' 5 2 **".split())
     assert list(s) == ["a", 25]
 
 
 # test swap
 def test_swap():
     s = RpnStack()
-    s.exec("'a' 5 2 swap")
+    s.exec(tokens="'a' 5 2 swap".split())
     assert list(s) == ["a", 2, 5]
 
 
 # test drop
 def test_drop():
     s = RpnStack()
-    s.exec("'a' 5 2 drop")
+    s.exec(tokens="'a' 5 2 drop".split())
     assert list(s) == ["a", 5]
 
 
 # test drop2
 def test_drop2():
     s = RpnStack()
-    s.exec("'a' 5 2 1 drop2")
+    s.exec(tokens="'a' 5 2 1 drop2".split())
     assert list(s) == ["a", 5]
 
 
 # test drpn
 def test_drpn():
     s = RpnStack()
-    s.exec("'a' 1 2 3 2 drpn")
+    s.exec(tokens="'a' 1 2 3 2 drpn".split())
     assert list(s) == ["a", 1]
 
 
 # test dup
 def test_dup():
     s = RpnStack()
-    s.exec("'a' 1 dup")
+    s.exec(tokens="'a' 1 dup".split())
     assert list(s) == ["a", 1, 1]
 
 
 # test pick
 def test_pick():
     s = RpnStack()
-    s.exec("'a' 'b' 1 pick")
+    s.exec(tokens="'a' 'b' 1 pick".split())
     assert list(s) == ["a", "b", "b"]
-    s.exec("3 pick")
+    s.exec(tokens="3 pick".split())
     assert list(s) == ["a", "b", "b", "a"]
 
 
 # test over
 def test_over():
     s = RpnStack()
-    s.exec("'a' 'b' 1 over")
+    s.exec(tokens="'a' 'b' 1 over".split())
     assert list(s) == ["a", "b", 1, "b"]
 
 
 # test rot
 def test_rot():
     s = RpnStack()
-    s.exec("1 2 3 rot")
+    s.exec(tokens="1 2 3 rot".split())
     assert list(s) == [2, 3, 1]
 
 
 # test roll as ROT
-def test_roll_3_is_rot():
-    s = RpnStack()
-    s.exec("'a' 'b' 'c' 3 roll")
-    assert list(s) == ["b", "c", "a"]
+def test_roll_3_is_rot(stack):
+    stack.exec(tokens="'a' 'b' 'c' 3 roll".split())
+    assert list(stack) == ["b", "c", "a"]
 
 
 # test roll
-def test_roll_n():
-    s = RpnStack()
-    s.exec("'a' 'b' 'c' 'd' 'e' 'f' 4 roll")
-    assert list(s) == ["a", "b", "d", "e", "f", "c"]
+def test_roll_n(stack):
+    stack.exec(tokens="'a' 'b' 'c' 'd' 'e' 'f' 4 roll".split())
+    assert list(stack) == ["a", "b", "d", "e", "f", "c"]
 
 
 # test roll as ROT
-def test_rolld_3_is_not_rot():
-    s = RpnStack()
-    s.exec("'a' 'b' 'c' 3 rolld")
-    assert list(s) == ["c", "a", "b"]
+def test_rolld_3_is_not_rot(stack):
+    stack.exec(tokens="'a' 'b' 'c' 3 rolld".split())
+    assert list(stack) == ["c", "a", "b"]
 
 
 # test dup2
-def test_dup2():
-    s = RpnStack()
-    s.exec("'a' 1 2 dup2")
-    assert list(s) == ["a", 1, 2, 1, 2]
+def test_dup2(stack):
+    stack.exec(tokens="'a' 1 2 dup2".split())
+    assert list(stack) == ["a", 1, 2, 1, 2]
 
 
 # test dupn
-def test_dupn():
-    s = RpnStack()
-    s.exec("'a' 1 2 3 4 5 3 dupn")
-    assert list(s) == ["a", 1, 2, 3, 4, 5, 3, 4, 5]
+def test_dupn(stack):
+    stack.exec(tokens="'a' 1 2 3 4 5 3 dupn".split())
+    assert list(stack) == ["a", 1, 2, 3, 4, 5, 3, 4, 5]
+
+
+def test_create_object_class(stack):
+    stack.exec("'object'")
+    stack.exec("eval")
+    assert list(stack) == [object]
+
+
+def test_create_object_instance(stack):
+    stack.exec("'object'")
+    stack.exec("eval")
+    stack.exec("()")
+    assert list(stack) == [object, tuple()]
+    stack.exec("call")
+    assert len(stack) == 1
+    assert list(stack)[0].__class__ == object
+
+
+def test_create_instance_with_params(stack):
+    stack.exec("'list'")
+    stack.exec("eval")
+    stack.exec("((1, 'b'), )")
+    assert list(stack) == [list, ((1, "b"),)]
+    stack.exec("call")
+    assert list(stack) == [[1, "b"]]
+
+
+def test_parse_complex_structure(stack):
+    stack.exec("{'a':123}, (2, 3, (4, 5, [], {}))")
+    output = stack.pop()
+    assert output == (dict(a=123), (2, 3, (4, 5, [], {})))
+
+
+def test_constants(stack):
+    stack.exec("e")
+    assert stack.pop() == np.e
+    stack.exec("pi")
+    assert stack.pop() == np.pi
+
+
+def test_exp_single_value(stack):
+    stack.exec("3")
+    stack.exec("exp")
+    assert stack.pop() == np.exp(3)
+
+
+def test_exp_array_value(stack):
+    stack.exec("(2, 1)")
+    stack.exec("exp")
+    output = stack.pop()
+    assert list(output == np.exp([2, 1])) == [True, True]
+    assert type(output) is np.ndarray
+
+
+def test_exp_complex(stack):
+    stack.exec("pi")
+    stack.exec("1.5j")
+    stack.exec("*")
+    stack.exec("exp")
+    assert np.isclose(stack.pop(), -1j)
+
+
+def test_sin_on_float(stack):
+    stack.exec("0")
+    stack.exec("sin")
+    assert stack.pop() == 0
+    stack.exec("pi")
+    stack.exec("2")
+    stack.exec("/")
+    stack.exec("sin")
+    assert stack.pop() == 1
+
+
+def test_sin_on_array(stack):
+    stack.exec("[0, 0.5, 1, 1.5, 2]")
+    stack.exec("pi")
+    stack.exec("multiply")
+    stack.exec("sin")
+    # close enough with 4 digits
+    output = stack.pop()
+    assert all(np.round(output, 4) == [0, 1, 0, -1, 0])
+    assert type(output) is np.ndarray
+
+
+def test_atan_on_float(stack):
+    # This test serves as template for all triginometric functions
+    stack.exec("(0, 1, -3)")
+    stack.exec("arctan")
+    output = stack.pop()
+    assert len(output) == 3
+    assert all(output == np.arctan([0, 1, -3]))
+    assert output[0] == 0
+    assert type(output) is np.ndarray
+
+
+def test_to_ndarray_ints(stack):
+    stack.exec("[0, 2, -3]")
+    stack.exec("array")
+    output = stack.pop()
+    assert output[0] == 0
+    assert output[1] == 2
+    assert output[2] == -3
+    assert type(output) is np.ndarray
+    assert type(output[2]) is np.int64
+
+
+def test_to_ndarray_floats_after_mutl(stack):
+    stack.exec("[0, 2, -3]")
+    stack.exec("array")
+    stack.exec("1.5")
+    stack.exec("*")
+    output = stack.pop()
+    assert output[2] == -4.5
+    assert type(output[2]) is np.float64
+    assert type(output[0]) is np.float64
+
+
+def test_to_ndarray_floats_from_values(stack):
+    stack.exec("[0.0, 2.1, -3]")
+    stack.exec("array")
+    output = stack.pop()
+    assert output[1]
+    assert type(output[0]) is np.float64
+
+
+@pytest.fixture
+def stack():
+    return RpnStack(raises=True)
